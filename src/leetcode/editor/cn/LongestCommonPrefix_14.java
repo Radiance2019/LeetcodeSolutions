166e0c7ff007c188bd7defc96192bd21fc25f6f1
Radiance2019 <iamis39500552@sina.com>
2020-08-01 17:35:41 +0800

    Initial commit

---
+++ src/leetcode/editor/cn/LongestCommonPrefix_14.java
@@ -0,0 +1,59 @@
+package leetcode.editor.cn;
+
+//编写一个函数来查找字符串数组中的最长公共前缀。 
+//
+// 如果不存在公共前缀，返回空字符串 ""。 
+//
+// 示例 1: 
+//
+// 输入: ["flower","flow","flight"]
+//输出: "fl"
+// 
+//
+// 示例 2: 
+//
+// 输入: ["dog","racecar","car"]
+//输出: ""
+//解释: 输入不存在公共前缀。
+// 
+//
+// 说明: 
+//
+// 所有输入只包含小写字母 a-z 。 
+// Related Topics 字符串 
+// 👍 1192 👎 0
+
+public class LongestCommonPrefix_14{
+    public static void main(String[] args) {
+        Solution solution = new LongestCommonPrefix_14().new Solution();
+            
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        int minLen = Integer.MAX_VALUE;
+        for (int i = 0; i < strs.length; i++) {
+            if (strs[i].length() < minLen) {
+                minLen = strs[i].length();
+            }
+        }
+        for (int i = 0; i < minLen; i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (strs[j].charAt(i) != c) {
+                    return sb.toString();
+                }
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
